00b62c8c85b7f3bd23e2e4d829b05d9d4530c91b
WebMohit20 <2003mohitkhemani@gmail.com>
2024-10-23 21:35:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+let container = document.querySelector(".container");
+
+for(let i=0;i<800;i++){
+	let box = document.createElement("div");
+	box.className = "square";
+	container.append(box);
+}
+let squares=document.querySelectorAll(".square");
+
+squares.forEach(sq=>{
+	sq.addEventListener("mouseover",()=>{
+		sq.style.backgroundColor=randomColor()
+	})
+})
+
+squares.forEach(sq=>{
+	sq.addEventListener("mouseout",()=>{
+		setTimeout(()=>{
+			sq.style.backgroundColor="transparent"
+		},1000)
+	})
+})
+
+function randomColor(){
+	let alnum="1234567890ABCDEF";
+	let color = "#";
+	
+	for(let i=0;i<6;i++){
+		color+=alnum[Math.floor(Math.random()*alnum.length)];
+	}
+	return color;
+}
